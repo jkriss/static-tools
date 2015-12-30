ee0aa6cf051fd08a5c6bfbda72a565013743a410
Jesse Kriss <jesse@jklabs.net>
2015-12-30 15:00:39 -0800

    Smarter server

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
-
 var express = require('express')
+var compression = require('compression')
 var serveStatic = require('serve-static')
-var app = express()
 var Path = require('path');
+var fs = require('fs');
+var isGzip = require('is-gzip-file');
+var app = express();
 
-var port = 3000;
+module.exports = function(port, path, callback) {
 
-app.use(serveStatic('public', {
-  setHeaders: setHeaders
-}))
+  app.use(compression());
+  app.use(serveStatic(path, {
+    extensions: ['html'],
+    setHeaders: setHeaders
+  }))
 
-function setHeaders(res, path) {
-  console.log("serving", path)
-  if (Path.extname(path) === '') {
-    res.setHeader('Content-Type', 'text/html')
+  function setHeaders(res, path) {
+    if (Path.extname(path) === '') {
+      res.setHeader('Content-Type', 'text/html')
+    }
+    // if it's fingerprinted, use that as the etag
+    if (Path.basename(path).match(/[0-9a-f]{32}-/)) {
+      var hash = Path.basename(path).split('-')[0];
+      res.setHeader('Etag', '"'+hash+'"');
+      res.setHeader('Cache-Control', 'public, max-age=2592000'); // 1 month
+    }
+    // if it's already a gzip file then say so
+    if (isGzip(path)) {
+      res.setHeader('Content-Encoding', 'gzip')
+    }
   }
-  // if it's fingerprinted, use that as the etag
-  if (Path.basename(path).match(/[0-9a-f]{32}-/)) {
-    var hash = Path.basename(path).split('-')[0];
-    res.setHeader('Etag', '"'+hash+'"');
-    res.setHeader('Cache-Control', 'public, max-age=2592000'); // 1 month
-  }
-  res.setHeader('Content-Encoding', 'gzip')
+
+  app.listen(port, callback);
+  console.log("Server running at http://localhost:" + port);
+  return app;
 }
 
-app.listen(port);
-console.log("listening at http://localhost:"+port);
+if (!module.parent) {
+  module.exports(3000, './public')
+}
