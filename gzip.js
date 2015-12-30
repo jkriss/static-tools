c5b653b6779b5a1b19a1cc8e295dd5864d1d6514
Jesse Kriss <jesse@jklabs.net>
2015-12-30 14:49:03 -0800

    Using is-gzip-file

--- gzip.js
+++ gzip.js
@@ -1,8 +1,7 @@
 var glob = require('glob');
 var fs = require('fs');
-var zlib = require("zlib");
-var readChunk = require('read-chunk');
-var GzipMagic = new Buffer([0x1f, 0x8b]); // gzip files start with this
+var zlib = require('zlib');
+var isGzip = require('is-gzip-file');
 
 module.exports = function() {
   glob('public/**/*', function(err, files) {
@@ -11,9 +10,8 @@ module.exports = function() {
     } else {
       for (i in files) {
         var file = files[i];
-        // if it's already a gzip file (based on magic number) then skip
-        var magicNumber = readChunk.sync(file, 0, 2);
-        if (!magicNumber.equals(GzipMagic)) {
+        // if it's already a gzip file then skip
+        if (!isGzip(file)) {
           // console.log("gzipping", file);
           var contents = fs.readFileSync(file, 'utf-8');
           fs.writeFileSync(file, zlib.gzipSync(contents), 'utf-8');
